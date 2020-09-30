f85b274d627354d570ad7c200580c6f57eed84f2
mikealessi24 <60523720+mikealessi24@users.noreply.github.com>
2020-09-30 13:09:46 -0400

    added links

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { Link } from "@reach/router";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <nav>
+        <Link to="/">Home</Link> <Link to="dashboard">Dashboard</Link>
+      </nav>
+    </div>
+  );
 }
 
 export default App;
